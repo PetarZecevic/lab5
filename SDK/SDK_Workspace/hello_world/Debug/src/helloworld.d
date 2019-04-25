6c187c0b1560144aa2b7d02bf30c0597180b1453
RadovanTausan <radovantausan67@gmail.com>
2019-04-25 11:44:05 +0200

    Moving text

--- SDK/SDK_Workspace/hello_world/Debug/src/helloworld.d
+++ SDK/SDK_Workspace/hello_world/Debug/src/helloworld.d
@@ -1,25 +1,49 @@
 src/helloworld.d: ../src/helloworld.c ../src/platform.h \
  ../src/platform_config.h \
+ ../../hello_world_bsp/microblaze_0/include/xintc.h \
+ ../../hello_world_bsp/microblaze_0/include/xil_types.h \
+ ../../hello_world_bsp/microblaze_0/include/xil_assert.h \
  ../../hello_world_bsp/microblaze_0/include/xparameters.h \
- ../../hello_world_bsp/microblaze_0/include/xio.h \
+ ../../hello_world_bsp/microblaze_0/include/xstatus.h \
  ../../hello_world_bsp/microblaze_0/include/xbasic_types.h \
- ../src/vga_periph_mem.h \
+ ../../hello_world_bsp/microblaze_0/include/xintc_l.h \
+ ../../hello_world_bsp/microblaze_0/include/xil_io.h \
+ ../../hello_world_bsp/microblaze_0/include/mb_interface.h \
+ ../../hello_world_bsp/microblaze_0/include/xil_printf.h \
+ ../../hello_world_bsp/microblaze_0/include/xparameters.h \
+ ../../hello_world_bsp/microblaze_0/include/xio.h ../src/vga_periph_mem.h \
  ../../hello_world_bsp/microblaze_0/include/xbasic_types.h \
  ../../hello_world_bsp/microblaze_0/include/xstatus.h \
  ../../hello_world_bsp/microblaze_0/include/xil_io.h \
- ../../hello_world_bsp/microblaze_0/include/xil_types.h \
- ../../hello_world_bsp/microblaze_0/include/mb_interface.h \
- ../../hello_world_bsp/microblaze_0/include/xil_printf.h
+ ../../hello_world_bsp/microblaze_0/include/xil_exception.h
 
 ../src/platform.h:
 
 ../src/platform_config.h:
 
+../../hello_world_bsp/microblaze_0/include/xintc.h:
+
+../../hello_world_bsp/microblaze_0/include/xil_types.h:
+
+../../hello_world_bsp/microblaze_0/include/xil_assert.h:
+
+../../hello_world_bsp/microblaze_0/include/xparameters.h:
+
+../../hello_world_bsp/microblaze_0/include/xstatus.h:
+
+../../hello_world_bsp/microblaze_0/include/xbasic_types.h:
+
+../../hello_world_bsp/microblaze_0/include/xintc_l.h:
+
+../../hello_world_bsp/microblaze_0/include/xil_io.h:
+
+../../hello_world_bsp/microblaze_0/include/mb_interface.h:
+
+../../hello_world_bsp/microblaze_0/include/xil_printf.h:
+
 ../../hello_world_bsp/microblaze_0/include/xparameters.h:
 
 ../../hello_world_bsp/microblaze_0/include/xio.h:
-
-../../hello_world_bsp/microblaze_0/include/xbasic_types.h:
 
 ../src/vga_periph_mem.h:
 
@@ -29,8 +53,4 @@ src/helloworld.d: ../src/helloworld.c ../src/platform.h \
 
 ../../hello_world_bsp/microblaze_0/include/xil_io.h:
 
-../../hello_world_bsp/microblaze_0/include/xil_types.h:
-
-../../hello_world_bsp/microblaze_0/include/mb_interface.h:
-
-../../hello_world_bsp/microblaze_0/include/xil_printf.h:
+../../hello_world_bsp/microblaze_0/include/xil_exception.h:
--- SDK/SDK_Workspace/hello_world/src/helloworld.c
+++ SDK/SDK_Workspace/hello_world/src/helloworld.c
@@ -38,19 +38,53 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "platform.h"
+#include "xintc.h"
 #include "xparameters.h"
 #include "xio.h"
 #include "vga_periph_mem.h"
+#include "xil_exception.h"
 
+XIntc Intc;
+Xint32 offset = 0;
+int interrupt_dir = 1;
+
+unsigned char string_s[] = "LPRS 2\n";
 
 void print(char *str);
+
+void my_timer_interrupt_handler(void * baseaddr_p) {
+	clear_text_screen(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR+offset);
+	if(offset >= 118 || offset <= -20)
+	{
+		interrupt_dir = (interrupt_dir == 1) ? -1 : 1; // promena smera.
+	}
+	print_string(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR+offset, string_s, 6);
+	offset += (Xint32)interrupt_dir;
+}
+
+void squareLeftRight()
+{
+	 int i, direction;
+	 i = 0;
+	 direction = 1;
+	 while(1)
+	 {
+		clear_graphics_screen(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR);
+		//if(condition(address, i, XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR))
+		if(i >= 33 || i <= -33)
+		{
+			direction = (direction == 1) ? -1 : 1; // promena smera.
+		}
+	 	move_square(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR, i);
+	 	i += direction;
+	 	delay(500000);
+	 }
+}
 
 int main()
 {
     init_platform();
-    unsigned char string_s[] = "LPRS 2\n";
-    int i;
-    int direction, address;
+    XStatus Status;
 
     VGA_PERIPH_MEM_mWriteMemory(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR + 0x00, 0x0);// direct mode   0
     VGA_PERIPH_MEM_mWriteMemory(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR + 0x04, 0x3);// display_mode  1
@@ -59,29 +93,46 @@ int main()
     VGA_PERIPH_MEM_mWriteMemory(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR + 0x10, 0xFFFFFF);// foreground 4
     VGA_PERIPH_MEM_mWriteMemory(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR + 0x14, 0x0000FF);// background color 5
     VGA_PERIPH_MEM_mWriteMemory(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR + 0x18, 0xFF0000);// frame color      6
-
+    VGA_PERIPH_MEM_mWriteMemory(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR + 0x1C, 480);// v_sync_counter_tc 7
+    VGA_PERIPH_MEM_mWriteMemory(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR + 0x20, 0x1);// enable interrupt 8
     print("Hello World\n\r");
 
     clear_text_screen(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR);
     clear_graphics_screen(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR);
-    draw_square(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR);
-    set_cursor(350);
-    print_string(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR, string_s, 6);
+    //draw_square(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR);
+    set_cursor(500);
+    //print_string(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR, string_s, 6);
 
-    direction = 1;
-    i = 0;
-    while(1){
-    	clear_graphics_screen(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR);
-    	//if(condition(address, i, XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR))
-    	if(i >= 33 || i <= -33)
-    	{
-    		direction = (direction == 1) ? -1 : 1; // promena smera.
-    	}
-    	move_square(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR, i);
-    	i += direction;
-    	delay(500000);
-    }
-	move_text(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR,1,string_s,6,6);
+	//move_text(XPAR_VGA_PERIPH_MEM_0_S_AXI_MEM0_BASEADDR,1,string_s,6,6);
 
+	//squareLeftRight();
+
+	//initialize interrupt controller
+	Status = XIntc_Initialize (&Intc, XPAR_INTC_0_DEVICE_ID);
+	if (Status != XST_SUCCESS)
+		xil_printf ("\r\nInterrupt controller initialization failure");
+	else
+		xil_printf("\r\nInterrupt controller initialized");
+
+	// Connect my_timer_interrupt_handler
+	Status = XIntc_Connect (&Intc, XPAR_INTC_0_DEVICE_ID, (XInterruptHandler) my_timer_interrupt_handler,(void *)0);
+
+	if (Status != XST_SUCCESS)
+		xil_printf ("\r\nRegistering MY_TIMER Interrupt Failed");
+	else
+		xil_printf("\r\nMY_TIMER Interrupt registered");
+
+	//start the interrupt controller in real mode
+	Status = XIntc_Start(&Intc, XIN_REAL_MODE);
+	//enable interrupt controller
+	XIntc_Enable (&Intc, XPAR_INTC_0_DEVICE_ID);
+
+	microblaze_enable_interrupts();
+
+	while(1){
+		;
+	}
+
+	cleanup_platform();
     return 0;
 }
